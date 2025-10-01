68bfbbc33c7831845cee82f72b99ddf2ac44c800
yunlin <yunlin@example.com>
2025-10-01 04:13:02 +0000

    docs: add author yunlin to README.md and base_export.h

--- base/base_export.h
+++ base/base_export.h
@@ -1,6 +1,7 @@
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+// Author: yunlin
 
 #ifndef BASE_BASE_EXPORT_H_
 #define BASE_BASE_EXPORT_H_
