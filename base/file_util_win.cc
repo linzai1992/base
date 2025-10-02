cb9d22035c1314c5ab5082e57e95c5af20936809
shoutao <shoutao@example.com>
2025-10-02 02:47:32 +0000

    docs(file_util): update author to shoutao in file_util_win.cc

--- base/file_util_win.cc
+++ base/file_util_win.cc
@@ -1,3 +1,7 @@
+// Copyright (c) 2024 shoutao. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "base/file_util.h"
 #include "base/logging.h"
 #include "base/utf_string_conversions.h"
